6e76e65debb8f150c84a85780e38831bb8efa9b2
pietro-sillano <pietrosillano@gmail.com>
2021-08-16 17:43:26 +0200

    update the readme.md

--- script_generator.py
+++ script_generator.py
@@ -1,10 +1,10 @@
 import os
 
 path='library_super_reduced' 
-bertpath='/home/wsojka00/Desktop/latin_bert' 
+bertpath='/latin_bert' 
+output='pickle'
 
 
-output='pickle_old'
 
 if not os.path.exists(output):
   os.mkdir(output)
@@ -21,7 +21,7 @@ for a,b,c in os.walk(path):
 		outfile=os.path.splitext(txt)[0]
 	
 		#print('python3 ./gen_file2.py --bertPath '+ bertpath +' --tokenizerPath '+ bertpath+ '/latin.subword.encoder -f '+ path +'/'+ b +'/' + txt +' -o  '+'/'+output+'/' +outfile , file = scriptfile )
-		print('python3 ./gen_file2.py --bertPath '+ bertpath +' --tokenizerPath '+ bertpath+ '/latin.subword.encoder -f '+ a +'/' + txt +' -o  '+'./'+output+'/' +outfile , file = scriptfile )
+		print('python3 ./text_to_pickle.py --bertPath '+ bertpath +' --tokenizerPath '+ bertpath+ '/latin.subword.encoder -f '+ a +'/' + txt +' -o  '+'./'+output+'/' +outfile , file = scriptfile )
 		i=i+1
 
 scriptfile.close()
